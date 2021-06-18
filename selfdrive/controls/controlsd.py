c0a05caa71d216b3015ff944a48969771ba7ab67
Aragon7777 <toms0760@gmail.com>
2021-06-18 13:18:59 -0400

    Allow 10mph over set speed without braking. Save your brake pads!
    Allow 10mph over set speed without braking. Save your brake pads!
    
    Update longitudinal_planner.py
    
    Update longcontrol.py
    
    Update longitudinal_planner.py
    
    Testing
    
    Update longitudinal_planner.py
    
    Update longitudinal_planner.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -445,8 +445,7 @@ class Controls:
     v_acc_sol = long_plan.vStart + dt * (a_acc_sol + long_plan.aStart) / 2.0
 
     # Gas/Brake PID loop
-    actuators.gas, actuators.brake = self.LoC.update(self.active, CS, v_acc_sol, long_plan.vTargetFuture, a_acc_sol, self.CP)
-
+    actuators.gas, actuators.brake = self.LoC.update(self.active, CS, v_acc_sol, long_plan.vTargetFuture, a_acc_sol, self.CP, long_plan.longitudinalPlanSource)
     # Steering PID loop and lateral MPC
     actuators.steer, actuators.steeringAngleDeg, lac_log = self.LaC.update(self.active, CS, self.CP, self.VM, params, lat_plan)
 
