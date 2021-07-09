dcc7ae392e7c4b8dad9e3dd228ed1ec2d906e2a8
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-07-09 10:43:49 -0400

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -591,7 +591,7 @@ class Controls:
     controlsState.canErrorCounter = self.can_error_counter
     
     if self.v_cruise_kph != 255:
-      controlsState.vCruise = controlsState.vCruise * 0.93
+      controlsState.vCruise = controlsState.vCruise * 1.01
 
     if self.CP.lateralTuning.which() == 'pid':
       controlsState.lateralControlState.pidState = lac_log
