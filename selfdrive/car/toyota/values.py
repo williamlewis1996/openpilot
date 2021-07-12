33f8b0f932a53fb7837919509fe8f2e9945ec867
Brett Sanderson <l-brett-l@hotmail.com>
2021-07-12 00:00:26 -0400

    Toyota: Separate LKAS/ACC

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -19,6 +19,16 @@ class CarControllerParams:
   STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
   STEER_ERROR_MAX = 350     # max delta between torque cmd and torque motor
 
+# Car button codes
+class CruiseButtons:
+  ACCEL_ACC = 9
+  DECEL_ACC = 10
+  SET_ACC = 8
+  ACCEL_CC = 6
+  DECEL_CC = 5
+  SET_CC = 1 
+  CANCEL = 0
+
 class CAR:
   PRIUS = "TOYOTA PRIUS 2017"
   PRIUS_TSS2 = "TOYOTA PRIUS TSS2 2021"
