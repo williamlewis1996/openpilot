c13597e3180d208f5206609e603f6117de0da4d1
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-07-28 22:58:43 -0400

    Smooth and Fast Acceleration

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -18,9 +18,10 @@ from selfdrive.swaglog import cloudlog
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 AWARENESS_DECEL = -0.2     # car smoothly decel at .2m/s^2 when user is distracted
-A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.2, 1.2, 0.8, 0.6]
-A_CRUISE_MAX_BP = [0., 15., 25., 40.]
+A_CRUISE_MIN = -2.0
+A_CRUISE_MAX_VALS = [1.0, 2.6, 2.5, 3.2]
+A_CRUISE_MAX_V_FOLLOWING = [1.0, 2.6, 2.5, 3.2]
+A_CRUISE_MAX_BP = [0., 15., 25., 35.]
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
