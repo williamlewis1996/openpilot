aa7d9d546d1da5c38267554f7be09f857aff6c54
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-08-04 20:31:15 -0400

    Read-only

--- .github/misc_scripts/mpc_tr.py
+++
@@ -1,9 +0,0 @@
-from cereal.messaging import SubMaster
-import time
-
-sm = SubMaster(['dynamicFollowData'])
-
-while True:
-  sm.update(0)
-  print('mpc_TR: {}'.format(sm['dynamicFollowData'].mpcTR))
-  time.sleep(1/20)
