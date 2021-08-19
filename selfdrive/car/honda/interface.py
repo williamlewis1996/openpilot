fa8dfa2cbe5bd5c8cc476ad86db8db57ef82fa2d
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-08-19 03:31:31 -0400

    DevUI v0.8.8

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -383,6 +383,7 @@ class CarInterface(CarInterfaceBase):
     ret.automaticLaneChange = self.CS.automaticLaneChange
     ret.belowLaneChangeSpeed = self.CS.belowLaneChangeSpeed
     ret.readdistancelines = self.CS.read_distance_lines
+    ret.engineRPM = self.CS.engineRPM
     
     buttonEvents = []
 
