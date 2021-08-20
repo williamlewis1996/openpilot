c4574ab3d027730208da82ca6202f49b1179929c
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-08-19 20:33:44 -0400

    Update ui.h

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -69,7 +69,7 @@ const Alert CONTROLS_UNRESPONSIVE_ALERT = {"TAKE CONTROL IMMEDIATELY", "Controls
                                            AudibleAlert::CHIME_WARNING_REPEAT};
 const int CONTROLS_TIMEOUT = 5;
 
-const int bdr_s = 30;
+const int bdr_s = 10;
 const int bdr_is = 30;
 const int header_h = 420;
 const int footer_h = 280;
