c84942f34ba2254d2f754f41927893f6359f171c
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-08-25 13:37:13 -0400

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -123,7 +123,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
   });
   mlButton->setFixedWidth(200);
   mlButton->setFixedHeight(200);
-  btns_layout->addWidget(mlButton, 0, Qt::AlignLeft);
+  btns_layout->addWidget(mlButton, 0, Qt::AlignRight | QT::AlignCenter);
 
   dfButton = new QPushButton("GAP\nAdjust");
   QObject::connect(dfButton, &QPushButton::clicked, [=]() {
