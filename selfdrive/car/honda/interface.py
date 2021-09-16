f08f6fdc0ff07742928ba24f1c8895d96b9b85e5
Will Lewis <will@orases.com>
2021-09-16 16:50:25 -0400

    Test Tune (0.35, 0.11)

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -130,7 +130,7 @@ class CarInterface(CarInterfaceBase):
         # modified filter output values:  0x009F, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0400, 0x0480
         # note: max request allowed is 4096, but request is capped at 3840 in firmware, so modifications result in 2x max
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560, 8000], [0, 2560, 3840]]
-        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.1]]
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.35], [0.11]]
       else:
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560], [0, 2560]]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[1.1], [0.33]]
@@ -424,7 +424,7 @@ class CarInterface(CarInterfaceBase):
       events.add(EventName.belowEngageSpeed)
 
     self.CS.disengageByBrake = self.CS.disengageByBrake or ret.disengageByBrake
-    
+
     if self.CS.CP.minEnableSpeed > 0 and ret.vEgo < 0.001:
       events.add(EventName.manualRestart)
 
@@ -440,7 +440,7 @@ class CarInterface(CarInterfaceBase):
     if not ret.brakePressed and not self.CS.brake_hold:
       self.CS.disengageByBrake = False
       ret.disengageByBrake = False
-    
+
     # handle button presses
     for b in ret.buttonEvents:
 
